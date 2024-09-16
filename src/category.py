dbeaab3b91e1978059a75064c445441a3e588ce5
Nikita Khramov <nikuser12321@gmail.com>
2024-09-16 15:54:17 +0300

    Small fixes

--- src/category.py
+++ src/category.py
@@ -5,7 +5,7 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
         self.products = products if products else []
