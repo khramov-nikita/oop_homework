33f087021620cd6c4772d41bd4a7570e2a042978
Nikita Khramov <nikuser12321@gmail.com>
2024-09-18 04:32:12 +0300

    Tasks 3 and 4

--- src/product.py
+++ src/product.py
@@ -11,5 +11,25 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def product_dict(self):
+        return {"name": self.name, "description": self.description, "price": self.__price, "quantity": self.quantity}
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float):
+        if new_price <= 0.0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
+
+    @classmethod
+    def new_product(cls, kwargs: dict):
+        return cls(**kwargs)
+
