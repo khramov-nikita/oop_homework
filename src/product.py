d46c3bb7666e6f34c0698922e7628bdbd264c0e3
Nikita Khramov <nikuser12321@gmail.com>
2024-09-30 22:26:20 +0300

    Added check for invalid quantity in Product

--- src/product.py
+++ src/product.py
@@ -45,6 +45,8 @@ class Product(MixinInfo, BaseProduct):
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
+        if quantity == 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
         self.name = name
         self.description = description
         self.__price = price
